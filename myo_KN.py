dfad30e165895359d26118ab0a54273a1d441370
45duro <jonathangc.awt@gmail.com>
2018-10-04 10:29:26 -0500

    Dejo exactamente lo que necesito, ya dispuesto a implantar el patch de los servos

--- myo_KN.py
+++ myo_KN.py
@@ -16,45 +16,71 @@ from myo_raw_jonathan import MyoRaw
 
 SUBSAMPLE = 3
 K = 15
-'''
-class NNClassifier(object):
-    #fabricacion de los archivos.dat con la informacion de
-    los datos de entrenamiento
 
-    def __init_(self):
-        
-        for i in range(10):
-            with open('vals%d.csv' % i, 'r') as f: pass
-        self.read_data()
 
-    def almacene_data(self, cls, vals):
-        with open('vals%d.dat' % cls, 'w') as f:
-            f.write(pack('8H', *vals))
-
-        #X_train,X_test,y_train,y_test = train_test_split(iris['data'], iris['target'])
-
-    def leer_data(self):
-        X=[]
-        Y=[]
-        ##entrenelo
-'''
-knn = 0
-clf = joblib.load('modeloEntrenado.pkl')
 
 class CLassificador(object):
     
+    clusters = 6
+    clf = joblib.load('modeloEntrenado.pkl')
     
     def __init__(self):
-        #knn = KNeighborsClassifier(n_neighbors=10)
         pass
+        
+        #for i in range(10):
+         #   with open('vals%d.dat' % i, 'ab') as f: pass
+        #self.read_data()
+        #knn = KNeighborsClassifier(n_neighbors=10)
+    
+    def store_data(self, target, vals):
+        with open("setDataCompleta.csv","a") as f:
+            
+            flag = str(vals) + "\t" + str(int(target)) + "\n"
+            flag = flag.replace("(","")
+            flag = flag.replace(")","")
+            f.write(flag)
+                
+            #f.close()
+
+            
+            #f.write(pack('8H', *vals))
+            #f.close()
+        #colocar Daros en los archivos
+        #self.train(np.vstack([self.X, vals]), np.hstack([self.Y, [cls]]))   
+    def limpiar_data(self):
+        with open("setDataCompleta.csv","w") as f:                      
+            f.write("")
+            f.close()
+    
+    def entrenar(self):
+        try:
+            print("A entrenar")
+            archivo = "setDataCompleta.csv"
+            df = pd.read_csv(archivo)
+        
+            arregloX = df[df.columns[:-1]].values
+            arregloy = df[df.columns[-1]].values  #as_matrix()
+            print(arregloX[len(arregloy)-2])
+            
+            X_train,X_test,y_train,y_test = train_test_split(arregloX, arregloy)
+            clf = KNeighborsClassifier(self.clusters)
+            #print(self.clusters)
+            clf.fit(X_train, y_train)      
+            print(clf.score(X_test, y_test))
+            ##Exportacion del modelo entrenado
+            joblib.dump(clf, 'modeloEntrenado.pkl')
+        except ValueError:
+            print("problemas para establecer el set de datos, intente de nuevo y Revise sus datos")
 
     def classify(self, d):
-        if self.X_test.shape[0] < K * SUBSAMPLE: return 0
-        return clf.predict([d])
-    
+        try:
+            return self.clf.predict([d])
+        except ValueError:
+            #print("El modelo esta mal creado o las etiquetas no son suficientes de acuerdo a los nodos")
+            return [0]
 
 class Myo(MyoRaw):
-    hist_len = 25
+    hist_len = 35
 
     def __init__(self, cls, tty=None):
         MyoRaw.__init__(self, tty)
@@ -68,15 +94,18 @@ class Myo(MyoRaw):
         self.pose_handlers = []
 
     def emg_handler(self, emg, moving):
-        y=emg
-        #y = self.cls.classify(emg)
+        y = self.cls.classify(emg)
         #Quita un dato 
         self.history_cnt[self.history[0]] -= 1
         #Coloca el siguiente
-        self.history_cnt[y] += 1
-        self.history.append(y)
+        #y[] = dato entero que importa
+        self.history_cnt[y[0]] += 1    
+        self.history.append(y[0])
+        #Tamaño del array siempre sera 25
         
+        ##Desempaqueta el mas comun en el historial
         r, n = self.history_cnt.most_common(1)[0]
+        
         if self.last_pose is None or (n > self.history_cnt[self.last_pose] + 5 and n > Myo.hist_len / 2):
             self.on_raw_pose(r)
             self.last_pose = r
@@ -95,17 +124,19 @@ if __name__ == '__main__':
     import subprocess
     m = Myo(CLassificador(), sys.argv[1] if len(sys.argv) >= 2 else None)
     m.add_raw_pose_handler(print)
-    
-    def page(pose):
-        if pose == 5:
-            subprocess.call(['xte', 'key Page_Down'])
-        elif pose == 6:
-            subprocess.call(['xte', 'key Page_Up'])
-
-    m.add_raw_pose_handler(page)
+  
+   
 
     
     m.connect()
 
-    while True:
-        m.run()
+    try:
+        while True:
+            m.run()
+        
+    except KeyboardInterrupt:
+        pass
+        
+    finally:
+        m.disconnect()
+        print()
