e60c21aa99ed0494cac57981aaef5499705bfec7
45duro <jonathangc.awt@gmail.com>
2018-10-25 11:02:43 -0500

    Carga de Datos 2 CSV prueva con conda y presicion de 38 a 92 puntos porcentuales

--- kmeansNoSuper.py
+++ kmeansNoSuper.py
@@ -3,7 +3,8 @@ import sklearn
 import pandas as pd
 
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+from sklearn import metrics
 from sklearn.externals import joblib
+
+datosMyo = 
